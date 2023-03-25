1a0c2f9c83fe6c8c62baafbd4ce3fed8b335f5e1
sunglimlee <steve.patriot@gmail.com>
2023-03-25 16:36:23 -0400

    16:36

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: '16:15'),
+      home: const MyHomePage(title: '16:36'),
     );
   }
 }
