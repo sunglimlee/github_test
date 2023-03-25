28c6878de67c5114939f1946399ea5714555dfca
sunglimlee <steve.patriot@gmail.com>
2023-03-25 16:37:07 -0400

    Revert "16:36"
    This reverts commit 1a0c2f9c83fe6c8c62baafbd4ce3fed8b335f5e1.

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: '16:36'),
+      home: const MyHomePage(title: '16:15'),
     );
   }
 }
